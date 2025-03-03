08cfffce4ad8d58ca4c9bd718c8df7dcd8ae9613
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-03-03 17:20:48 -0500

    Added recent Library version

--- SuperSecretLib5000.py
+++ SuperSecretLib5000.py
@@ -1,212 +1,243 @@
-from hub import light_matrix, motion_sensor, port
+from hub import light, light_matrix, motion_sensor, port
+
 import color
 import color_sensor
-import motor_pair
-import motor
-import runloop
 
+import motor_pair as mpair
+
+import runloop
 
 import time
 import math
 import sys
 
+# Some constants used for movement calculations...
+WHEEL_DIAMETER = 6.1
+TURN_RADIUS = 5
+F_CORRECTION_CONST = 0.001  # For forward move: Control by this number for every degree of error.
 
-def ssl5k_ver():
+# Settings...
+turn_assist = False
+forward_assist = False
+
+
+def settings_light():
+    if turn_assist and forward_assist:
+        light.color(light.CONNECT, color.GREEN)
+    elif turn_assist:
+        light.color(light.CONNECT, color.YELLOW)
+    elif forward_assist:
+        light.color(light.CONNECT, color.AZURE)
+
+
+def gyro_light(status: str):
+    if status.lower() == "good":
+        light.color(light.POWER, color.GREEN)
+    if status.lower() == "bad":
+        light.color(light.POWER, color.RED)
+
+
+def light_power_off():
+    light.color(light.POWER, color.AZURE)
+
+
+def reset_gyro():
+    motion_sensor.set_yaw_face(motion_sensor.TOP)
+    motion_sensor.reset_yaw(0)
+    yaw = motion_sensor.tilt_angles()
+    print(yaw)
+
+
+def ver():
     print(sys.version)
-    print("Welcome to SuperSecretLib5000!")
-    print("ssl5k ver 1.3.CHAMPS")
+    print("ssl5k - ver 2.0-AZURE")
+    print("Welcome to SuperSecretLib5000.py!")
+    print("Created by FLL Team #3249 - the CaptiBytes")
 
 
-# Constants used in calculations
-d = 6.1# Diameter of a single wheel (OLD)
-r = 5# Pivot Radius (OLD)
+def init(drive_l, drive_r, attach_l, attach_r, sensor_l, sensor_r):
+    x = None
+    ver()
+    try:
+        try:
+            mpair.unpair(mpair.PAIR_1)
+            mpair.unpair(mpair.PAIR_2)
+        except:
+            print("No pairs found, creating new motor pairs...")
+        mpair.pair(mpair.PAIR_1, drive_l, drive_r)
+        mpair.pair(mpair.PAIR_2, attach_l, attach_r)
+    except:
+        print("Something went wrong! Cannot create motor pairs!")
+    light.color(light.POWER, color.AZURE)
+    settings_light()
+    light_matrix.write("#")
+    return x, drive_l, drive_r, attach_l, attach_r, sensor_l, sensor_r
 
 
-async def forward(dist:float,stop:bool=False,run_until=None,**kwargs):
-    '''Move bot forwards by a number of centimeters.
-
-
-    - `dist: float:` The distance in centimeters
-    - `stop: bool:` Controls whether or not motors will be issued a stop command at the end of the function.
-    - `**kwargs: dict[str, Unknown]:` Allows the user to pass velocity and acceleration/deceleration arguments optionally.
-
-
-    Examples:
-    ```
-        forward(46)    # Move forwards 46 centimeters
-        forward(-23)    # Move backwards
-        forward(11,True) # Move forwards and stop at the end
-    ```
-    '''
+async def forward(dist: int, stop: bool = False, run_until=None,**kwargs):
     if run_until is None:
         run_until = lambda: False
+        limit = False
+    else:
+        limit = True
 
+    reset_gyro()
+    yaw = None
 
-    degrees = dist*(360.0/(math.pi*d))
+    degrees = dist*(360.0/(math.pi*WHEEL_DIAMETER))
 
-
-    acceleration = kwargs.get("acceleration", False)
-    velocity = kwargs.get("velocity",360)
-    if (degrees < 0):
+    accel = kwargs.get("acceleration", 10000)
+    velocity = kwargs.get("velocity", 360)
+    if degrees < 0:
         velocity = -1*velocity
-
 
     run_time = round((degrees/velocity)*1000.0)
     run_time = abs(run_time)
 
+    print("""\nForward --
+    dist= {dist}, stop= {stop}, run_until= {run_until}
+    run_time= {run_time}
+    velocity= {velocity}
+    accel= {accel}
+    """
+    .format(dist = dist, stop = stop, run_until = limit, run_time = run_time, velocity = velocity, accel = accel))
 
-    print("dist:{dist}, degrees:{degrees}, velocity:{velocity}, time:{time}"
-    .format(dist=dist,degrees=degrees,velocity=velocity,time=time))
+    if forward_assist:
+        print("Forward: Using gyro to assist movement...")
+        start = time.ticks_ms()
+        while True:
+            if run_until():
+                print("Forward: Stopping due to sensors...")
+                break
+            now = time.ticks_ms()
+            if now-start > run_time:
+                print("Forward: Stopping due to time...")
+                break
+
+            yaw = motion_sensor.tilt_angles()[0]
+            f_correction_factor = yaw*F_CORRECTION_CONST
+            correction = velocity*f_correction_factor
+            mpair.move_tank(mpair.PAIR_1, int(velocity+correction), int(velocity-correction), acceleration=accel)
+            gyro_light("good")
+            
+    else:  # (if forward_assist is NOT True):
+        print("Forward: Moving without gyro assist...")
+        mpair.move_tank(mpair.PAIR_1, velocity, velocity, acceleration=accel)
+        start = time.ticks_ms()
+        while True:
+            if run_until():
+                print("Forward: Stopping due to sensors...")
+                break
+            now = time.ticks_ms()
+            if now-start > run_time:
+                print("Forward: Stopping due to time...")
+                break
+
+    if stop:
+        mpair.stop(mpair.PAIR_1)
+
+    light_power_off()
 
 
-    motor_pair.move(motor_pair.PAIR_1,0,velocity=velocity,acceleration=acceleration)
-    start = time.ticks_ms()
-    while True:
-        if run_until():
-            break
-        now = time.ticks_ms()
-        if now-start > run_time:
-            break
-
-
-    if (stop == True):
-        motor_pair.stop(motor_pair.PAIR_1)
-
-
-async def turn(theta:int,stop:bool=False,run_until=None,**kwargs):
-    '''Turn bot by a number of degrees.
-
-
-    - `theta: int:` The angle that the bot will turn.
-    - `stop: bool:` Controls whether or not motors will be issued a stop command at the end of the function.
-    - `**kwargs: dict[str, Unknown]:` Allows the user to pass velocity and acceleration/deceleration arguments optionally.
-
-
-    Examples:
-    ```
-        ssl5k.turn(90)    # Turn 90° right.
-        ssl5k.turn(-90)    # Turn 90° left.
-        ssl5k.turn(180,True) # Turn 180° right and stop at the end.
-    ```
-    '''
+async def turn(theta: int, stop: bool = False, run_until=None, **kwargs):
     if run_until is None:
         run_until = lambda: False
+        limit = False
+    else:
+        limit = True
 
+    reset_gyro()
 
-    motion_sensor.reset_yaw(0)
+    dist = (2*math.pi*TURN_RADIUS)*(theta/360.0)
+    degrees = dist*(360.0/(math.pi*WHEEL_DIAMETER))
 
-
-    dist = (2*math.pi*r)*(theta/360.0) # r is now 5.5
-    degrees = dist*(360.0/(math.pi*d))
-
-
-    velocity = int(kwargs.get("velocity",360)/2) # Half velocity for tank movement.
-    if (degrees < 0):
+    velocity = int(kwargs.get("velocity", 360)/2)
+    if degrees < 0:
         velocity = -1*velocity
 
+    run_time = round((degrees/velocity)*1000.0)
+    run_time = abs(run_time)
 
-    run_time = round((degrees/velocity)*1000)
-    run_time = abs(run_time) # Ensures that time is greater than 0
+    print("""\nTurn --
+    theta= {theta}, stop= {stop}, run_until= {run_until}
+    kwargs= {kwargs}
+    """
+    .format(theta = theta, stop = stop, run_until = limit, kwargs = kwargs.items()))
+
+    total_turn = 0.0
+
+    if turn_assist is False:
+        mpair.move_tank(mpair.PAIR_1, velocity, -1*velocity)
+        start = time.ticks_ms()
+        while True:
+            if run_until():
+                print("Turn: Stopping due to sensors...")
+                break
+            now = time.ticks_ms()
+            if now-start > run_time:
+                print("Turn: Stopping due to time...")
+                break
+
+    elif turn_assist:
+        mpair.move_tank(mpair.PAIR_1, velocity, -1*velocity)
+        start = time.ticks_ms()
+        angle_prev = (motion_sensor.tilt_angles()[0])/10.0
+        while True:
+            if run_until():
+                break
+            now = time.ticks_ms()
+            if now-start > run_time:
+                break
+
+            angle_now = (motion_sensor.tilt_angles()[0])/10.0
+            if angle_now < 0.0 and angle_prev > 0.0 and theta > 0.0:  # Sign change + -> - (turning right)
+                _delta_pos = 180.0 - angle_prev
+                _delta_neg = angle_now + 180.0
+                delta_turn = _delta_pos + _delta_neg
+            elif angle_now > 0.0 and angle_prev < 0.0 and theta < 0.0:
+                _delta_neg = -180.0 - angle_prev
+                _delta_pos = angle_now + 180.0
+                delta_turn = _delta_pos + _delta_neg
+            else:
+                delta_turn = angle_now - angle_prev
+
+            total_turn += delta_turn
+
+            if abs(total_turn) >= abs(theta):
+                break
+
+            angle_prev = angle_now
+
+        print("""\nGyro --
+    expected= {theta} degrees
+    gyro= {total_turn} degrees
+    error= {error} degrees
+        """
+        .format(theta = theta, total_turn = total_turn, error = total_turn-theta))
+
+    if stop:
+        mpair.stop(mpair.PAIR_1)
+
+    light_power_off()
 
 
-    print("""DEBUG:
-    -[INPUT]- theta: {theta}, velocity: {velocity}, stop: {stop}
-    -[OUTPUT]- dist: {dist}, degrees: {degrees}, time: {time}"""
-    .format(theta=theta, velocity=velocity, stop=stop, dist=dist, degrees=degrees, time=time))
+async def attachment(degrees: int, attach_port: str):
+    if attach_port.lower() == "left":
+        await mpair.move_tank_for_degrees(mpair.PAIR_2, degrees, 360, 0)
+    if attach_port.lower() == "right":
+        await mpair.move_tank_for_degrees(mpair.PAIR_2, degrees, 0, 360)
+    if attach_port.lower() == "both":
+        await mpair.move_tank_for_degrees(mpair.PAIR_2, degrees, 360, 360)
+
+    print("""\nAttachment --
+    degrees= {degrees}
+    attach_port= {attach_port}
+    """
+        .format(degrees = degrees, attach_port = attach_port))
 
 
-    motor_pair.move_tank(motor_pair.PAIR_1,velocity,-1*velocity)
-    start = time.ticks_ms()
-    while True:
-        if run_until():
-            break
-        now = time.ticks_ms()
-        if now-start > run_time:
-            break
-
-
-    if (stop == True):
-        motor_pair.stop(motor_pair.PAIR_1)
-
-
-    yaw = -1*motion_sensor.tilt_angles()[0]
-    print("TURN INFO: theta: {theta}, yaw: {yaw}, error: {error}"
-    .format(theta=theta,yaw=yaw,error=yaw-theta))
-
-
-async def attachment(degrees:int,attach_port:str):
-    '''Run attachment motor for a number of degrees.
-
-
-    - `degrees: int:` Number of degrees to run the motor for.
-    - `attach_port: str:` Attachment motor to run. Left/Right/Both
-
-
-    Examples:
-    ```
-        attachment(30,"both")
-        attachment(360,"left")
-        attachment(-360,"right")
-    ```
-    '''
-
-
-    if attach_port == "left" :
-        await motor_pair.move_tank_for_degrees(motor_pair.PAIR_2,degrees,360,0)
-    if attach_port == "right":
-        await motor_pair.move_tank_for_degrees(motor_pair.PAIR_2,degrees,0,360)
-    if attach_port == "both" :
-        await motor_pair.move_tank_for_degrees(motor_pair.PAIR_2,degrees,360,360)
-
-
-    print("Attachment: {degrees}, {attach_port}".format(degrees=degrees,attach_port=attach_port))
-
-
-async def sensor(sensor_type:str,sensor_port:int,expected:int):
-    '''Checks for an expected value until the value is met.
-
-
-    - `sensor_type: str:` The type of sensor used. Color/Distance/Force/etc.
-    - `sensor_port: str:` Sensor to detect with. Left/Right/Both
-    - `expected_value: int:` The value you are checking for.
-
-
-    Examples:
-    ```
-        black = sensor("color","both",0) # black will be 'False' until the color sensors see black
-        yellow = sensor("color","left",7) # yellow will be 'False' until the left color sensor sees yellow
-    ```
-    '''
-   
+async def sensor(sensor_type: str, sensor_port: int, expected: int):
     if sensor_type.lower() == "color":
         value = color_sensor.color(sensor_port)
         return value == expected
-    # We only have color sensors implemented at the moment..
-
-
-# Setup function to automatically create motor pairs:
-def init(drive_left,drive_right,attach_left,attach_right,sensor_left,sensor_right):
-    ssl5k_ver()
-    motor_pair.pair(motor_pair.PAIR_1,drive_left,drive_right)
-    motor_pair.pair(motor_pair.PAIR_2,attach_left,attach_right)
-    return drive_left,drive_right,attach_left,attach_right,sensor_left,sensor_right
-
-
-async def test(testid: str):
-    if (__name__ == "__main__"):
-        if (testid == "turns"):
-            ports = init(port.C,port.F,port.B,port.D,port.A,port.E)
-            print("Left wheel: {0}, Right wheel: {1}, Left attachment motor: {2}, Right attachment motor: {3}, Left sensor: {4}, Right Sensor: {5}"
-            .format(ports[0],ports[1],ports[2],ports[3],ports[4],ports[5]))
-            await attachment(-360,"right")
-            _run_until = lambda: sensor("color",ports[4],0)
-            await forward(50,True,run_until=_run_until)
-            #await turn(90,True)
-            #await turn(-90,True)
-            #await turn(180,True)
-            #await turn(-180,True)
-            #await turn(360,True)
-            #await turn(-360,True)
-
-
-runloop.run(test('turns'))
+    # We only have color sensors implemented at the moment.
