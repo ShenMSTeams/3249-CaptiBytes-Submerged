994a58702728030997bc6c64d9a88588c8cbc5be
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-03-15 10:14:04 -0400

    Delete sample_main.py
    For a few reasons:
    1. Importing this file blindly via wildcard (*) would mean not knowing what you were importing, and I don't want to promote that.
    2. Not importing this file blindly would take up more lines than just importing SuperSecretLib5000 and Runloop.
    
    The only reason for not deleting this file is if a user forgets what to import and just imports main, which would lead to reason 1 (not good).

--- sample_main.py
+++
@@ -1,8 +0,0 @@
-try:
-    import SuperSecretLib5000 as ssl5k
-    from hub import port
-    import runloop
- 
-    print("Imported (ssl5, hub.port, runloop) successfully!")
-except Exception:
-    print("Failed to import modules...")
