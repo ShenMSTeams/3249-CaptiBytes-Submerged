799fb72ec1a7bc15f45c40220066da11113fcf01
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-08 01:42:45 -0400

    Improve simple code styling

--- Worlds_Code/4_Red_Neptune.py
+++ Worlds_Code/4_Red_Neptune.py
@@ -11,14 +11,14 @@ import runloop
 ssl5k.init(use_json=True)
 
 async def run():
-    await ssl5k.turn(-34,True)
-    await ssl5k.forward(60,True, assit=True)
-    await ssl5k.turn(-29,True)
-    await ssl5k.forward(20,True, assit=True)
-    await ssl5k.forward(-4,True, assit=True)
-    await ssl5k.turn(-25,True)
-    await ssl5k.attachment(500, "Right", velocity=-500)
-    await ssl5k.forward(-4,True, assit=True)
-    await ssl5k.attachment(400, "Right", velocity=300)
+    await ssl5k.turn(-34, True)
+    await ssl5k.forward(60, True)
+    await ssl5k.turn(-29, True)
+    await ssl5k.forward(20, True)
+    await ssl5k.forward(-4, True)
+    await ssl5k.turn(-25, True)
+    await ssl5k.attachment(500, "right", velocity=-500)
+    await ssl5k.forward(-4, True)
+    await ssl5k.attachment(400, "right", velocity=300)
   
 runloop.run(run())
