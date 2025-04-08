7bb27f754463476cf7ab2ea87d1a46063780e6b0
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-08 01:45:58 -0400

    Improve simple code styling

--- Worlds_Code/2_Red_Razor.py
+++ Worlds_Code/2_Red_Razor.py
@@ -16,7 +16,7 @@ async def main():
     await ssl5k.forward(47, True, assist=True)
     await ssl5k.turn(-83, True)
     await ssl5k.forward(15, True, assist=True)
-    await ssl5k.attachment(200, "right", valocity=100)
+    await ssl5k.attachment(200, "right")
     await ssl5k.forward(-15, True, assist=True)
     await ssl5k.turn(-90, True)
     await ssl5k.forward(25, True, assist=True)
