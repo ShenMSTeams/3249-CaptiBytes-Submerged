977b44af9a4236d97e5a509981e1aeb4740c131a
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-08 01:04:22 -0400

    Update 1_Red_UselessClaw.py

--- Worlds_Code/1_Red_UselessClaw.py
+++ Worlds_Code/1_Red_UselessClaw.py
@@ -15,7 +15,6 @@ import runloop
 ssl5k.init(use_json=True)
 
 async def main():
-
     await ssl5k.forward(13,True, velocity=1000)
     await ssl5k.attachment(-400, "right", velocity=2000)
     await ssl5k.forward(-100, True, velocity=2000)
