1fdc7f404621cb8e7b17b8f3fbaa081936156553
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-07 23:59:59 -0400

    Update 1_Red_UselessClaw.py

--- Worlds_Code/1_Red_UselessClaw.py
+++ Worlds_Code/1_Red_UselessClaw.py
@@ -1,14 +1,18 @@
-from hub import port
+''' Code slot 1, Red Base, uses 'Useless Claw' attachment.
+This program does not complete any missions on its own.
+Instead, it:
+'''
+
 import SuperSecretLib5000 as ssl5k
 import runloop
 
 ssl5k.init(use_json=True)
 
-async def ripuselessstick():
+async def main():
 
     await ssl5k.forward(13,True, velocity=1000)
     await ssl5k.attachment(-400, "right", velocity=2000)
     await ssl5k.forward(-100, True, velocity=2000)
     await ssl5k.attachment(360, "right")
 
-runloop.run(ripuselessstick())
+runloop.run(main())
