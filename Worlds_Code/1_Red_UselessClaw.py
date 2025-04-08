8f8745be2cdf5923a739712b23f06de5e6095cc3
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-08 01:46:29 -0400

    Improve simple code styling

--- Worlds_Code/1_Red_UselessClaw.py
+++ Worlds_Code/1_Red_UselessClaw.py
@@ -15,7 +15,7 @@ import runloop
 ssl5k.init(use_json=True)
 
 async def main():
-    await ssl5k.forward(13,True, velocity=1000)
+    await ssl5k.forward(13, True, velocity=1000)
     await ssl5k.attachment(-400, "right", velocity=2000)
     await ssl5k.forward(-100, True, velocity=2000)
     await ssl5k.attachment(360, "right")
