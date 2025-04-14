7aa0248d523350da8ef9a511814470aebe0ef60a
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 16:34:57 -0400

    Update and rename 7_Blu_BalaenaPrandium.py to 7_Blu_Spatula.py

---
+++ Worlds_Code/7_Blu_Spatula.py
@@ -0,0 +1,25 @@
+import SuperSecretLib5000 as ssl5k
+import runloop
+
+ssl5k.init(use_json=True)
+
+async def main():
+    await ssl5k.forward(10,True,assist=True)
+    await ssl5k.turn(-37,True)
+    await ssl5k.forward(58,True, assist=True)
+    await ssl5k.attachment(200,"left", velocity=360)
+    await ssl5k.attachment(-900,"left", velocity=-360)
+    await ssl5k.attachment(200,"left", velocity=630)
+    await ssl5k.forward(-10, True)
+    await ssl5k.turn(-25,True)
+    await ssl5k.attachment(-200,"right", velocity=630)
+    await ssl5k.forward(-8, True)
+    ssl5k.attachment(-230,"right", velocity=-630)
+    await ssl5k.attachment(800,"right", velocity=-1500)
+    await ssl5k.turn(-20, True)
+    await ssl5k.attachment(200,"right", velocity=-1500)
+    await ssl5k.forward(20,True, velocity=-6600)
+    await ssl5k.turn(50, True)
+    await ssl5k.forward(-50, velocity=700)
+
+runloop.run(main())
