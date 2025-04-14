9dd3b3cbaf56962d239b2cb43ed7c49cb671c105
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 16:14:37 -0400

    Update 0_Red_Kriller.py

--- Worlds_Code/0_Red_Kriller.py
+++ Worlds_Code/0_Red_Kriller.py
@@ -4,13 +4,7 @@ import runloop
 ssl5k.init(use_json=True)
 
 async def main():
-    await ssl5k.forward(20, True, assist=True)
-    await ssl5k.turn(-30, True)
-    await ssl5k.forward(44, True, assist=True)
-    await ssl5k.turn(82, True)
-    await ssl5k.forward(30, True, assist=True)
-    await ssl5k.forward(-30, True, assist=True)
-    await ssl5k.turn(-100, True)
-    await ssl5k.forward(-65, True, assist=True)
+ await ssl5k.forward(80,True, assist=True, velocity=700)
+ await ssl5k.forward(-100,True, assist=True, velocity=700)
 
 runloop.run(main())
