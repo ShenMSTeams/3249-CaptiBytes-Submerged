0375e850188970db9ea1e0832ab7fa6746ae4ba7
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 15:42:33 -0400

    Create 4_Red_Neptune.py

---
+++ Worlds_Code/4_Red_Neptune.py
@@ -0,0 +1,18 @@
+import SuperSecretLib5000 as ssl5k
+import runloop
+
+ssl5k.init(use_json=True)
+
+async def main():
+    await ssl5k.forward(55, True, velocity=1000)
+    await ssl5k.turn(-50, True)
+    await ssl5k.forward(6, True, velocity=1000)
+    await ssl5k.attachment(-300, "left", velocity=1000)
+    await ssl5k.forward(-18, True, velocity=1000)
+    await ssl5k.attachment(-500, "left", velocity=-1000)
+    await ssl5k.forward(26, True, velocity=1000)
+    await ssl5k.forward(-22, True, velocity=1000)
+    await ssl5k.turn(-130, True)
+    await ssl5k.forward(70, True, velocity=1000)
+
+runloop.run(main())
