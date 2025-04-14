a53dd2a86fca035945db39268bf8620fa38ed9c0
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 15:53:35 -0400

    Create 1_Red_UselessClaw.py

---
+++ Worlds_Code/1_Red_UselessClaw.py
@@ -0,0 +1,12 @@
+import SuperSecretLib5000 as ssl5k
+import runloop
+
+ssl5k.init(use_json=True)
+
+async def main():
+    await ssl5k.forward(13, True, velocity=1000)
+    await ssl5k.attachment(-400, "right", velocity=2000)
+    await ssl5k.forward(-100, True, velocity=2000)
+    await ssl5k.attachment(360, "right")
+
+runloop.run(main())
