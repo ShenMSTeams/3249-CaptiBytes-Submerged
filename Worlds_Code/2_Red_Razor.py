1632d3fd46090875018c5e77e42de5fa451e566c
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 15:46:18 -0400

    Create 2_Red_Razor.py

---
+++ Worlds_Code/2_Red_Razor.py
@@ -0,0 +1,19 @@
+import SuperSecretLib5000 as ssl5k
+import runloop
+
+ssl5k.init(use_json=True)
+
+async def main():
+    await ssl5k.forward(30, True, assist=True)
+    await ssl5k.turn(-90, True)
+    await ssl5k.forward(47, True, assist=True)
+    await ssl5k.turn(-83, True)
+    await ssl5k.forward(15, True, assist=True)
+    await ssl5k.attachment(200, "right")
+    await ssl5k.forward(-15, True, assist=True)
+    await ssl5k.turn(-90, True)
+    await ssl5k.forward(25, True, assist=True)
+    await ssl5k.turn(45, True)
+    await ssl5k.forward(40, True, assist=True)
+
+runloop.run(main())
