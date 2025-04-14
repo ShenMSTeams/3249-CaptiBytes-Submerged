574fcc124b9674ccd3a42c22240542649a87dbae
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 14:39:28 -0400

    Create 8_Blu_Crabby.py

---
+++ Worlds_Code/8_Blu_Crabby.py
@@ -0,0 +1,22 @@
+import SuperSecretLib5000 as ssl5k
+import runloop
+
+ssl5k.init(use_json=True)
+
+async def main():
+    await ssl5k.turn(3)
+    await ssl5k.forward(46.7, True, assist=True)
+    await ssl5k.attachment(620, "right", velocity=-4000)
+    await ssl5k.forward(-5, True)
+    await ssl5k.attachment(500, "right", velocity=1000)
+    await ssl5k.forward(14, True)
+    await ssl5k.attachment(700, "right", velocity=-4000)
+    await ssl5k.forward(-5, True)
+    await ssl5k.attachment(700, "right", velocity=4000)
+    await ssl5k.turn(-10)
+    await ssl5k.forward(5, True)
+    await ssl5k.attachment(700, "right", velocity=-4000)
+    await ssl5k.turn(-20)
+    await ssl5k.forward(-74, True, assist=True)
+
+runloop.run(ripuselessstick())
