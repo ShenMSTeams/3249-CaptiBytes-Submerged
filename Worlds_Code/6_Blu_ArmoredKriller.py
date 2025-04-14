6cc1a1231ca7208eda489fec4285a5de065b7866
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 15:39:22 -0400

    Create 6_Blu_ArmoredKriller.py

---
+++ Worlds_Code/6_Blu_ArmoredKriller.py
@@ -0,0 +1,20 @@
+import SuperSecretLib5000 as ssl5k
+import runloop
+
+ssl5k.init(use_json=True)
+
+async def squidkid():
+    await ssl5k.forward(40,True, assist=True, velocity=700)
+    await ssl5k.forward(-50,True, assist=True, velocity=700)
+
+async def sampler():
+    await ssl5k.forward(20, True, assist=True)
+    await ssl5k.turn(-30, True)
+    await ssl5k.forward(44, True, assist=True)
+    await ssl5k.turn(82, True)
+    await ssl5k.forward(30, True, assist=True)
+    await ssl5k.forward(-30, True, assist=True)
+    await ssl5k.turn(-100, True)
+    await ssl5k.forward(-65, True, assist=True)
+
+runloop.run(squidkid())
