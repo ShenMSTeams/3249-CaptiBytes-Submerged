9d6b634253c95d0bb64859e5c4ff9b9522042fcf
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-04-14 16:25:36 -0400

    Update 9_Blu_Squiddler.py

--- Worlds_Code/9_Blu_Squiddler.py
+++ Worlds_Code/9_Blu_Squiddler.py
@@ -1,3 +1,10 @@
+''' This code completes the following missions: 
+- "M10 - Submersible", 
+- "M09 - Unexpected Encounter",
+- "M05 - Angler Fish" 
+
+This Run gets a total of 80 Pts.
+'''
 import SuperSecretLib5000 as ssl5k
 import runloop
 
@@ -5,7 +12,7 @@ ssl5k.init(use_json=True)
 
 async def main():
     await ssl5k.forward(-5)
-    await ssl5k.attachment(60,"right")
+    await ssl5k.attachment(60, "right")
     await ssl5k.forward(46, False, assist=True)
     await ssl5k.turn(35, True)
     await ssl5k.forward(5.7, True) 
